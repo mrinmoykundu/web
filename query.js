eda8b5f271bd591e4d4167cbf7c7346c0a3a33f9
mrinmoykundu <52355567+mrinmoykundu@users.noreply.github.com>
2020-09-10 22:35:42 +0600

    added time

--- query.js
+++ query.js
@@ -5,6 +5,7 @@ function query(){
 
 	var d = document.getElementsByClassName('Date')
 	var expired = document.getElementsByClassName('expired')
+	var time_remaining = document.getElementsByClassName('time_remaining')
 
 	for(let i = 0; i < d.length; i++){
 		list = d[i].innerText.split('/');
@@ -12,6 +13,14 @@ function query(){
 
 		if(today >= dead_line){
 		 expired[i].style.display = 'inline';
+		 time_remaining[i].style.display = 'none';
+		}else{
+			days_remaining = (dead_line - today)/(1000*60*60*24);
+			hours = (days_remaining - Math.floor(days_remaining))*24;
+			minutes = (hours - Math.floor(hours))*60;
+			time_remaining[i].innerHTML = 'Time Remaing ' + Math.floor(days_remaining)
+			 + ' days ' + Math.floor(hours) + ' hours ' + Math.floor(minutes) +
+			 ' minutes ';
 		}
 	}
 }
